bc93ee4555fa97f1bfccb1c05d34062ab99f832f
turkjr19 <37075884+turkjr19@users.noreply.github.com>
2021-04-23 11:46:01 -0400

    Add files via upload
    scrape file of durham region covid data tracker powerbi site

--- covid_durham.R
+++ covid_durham.R
@@ -85,10 +85,6 @@ sheet_write(weeklyCaseData, ss2, date$date)
 pivot_data_table_2 <- read_html(remDr$getPageSource()[[1]]) %>%
   querySelector("#pvExplorationHost > div > div > exploration > div > explore-canvas-modern > div > div.canvasFlexBox > div > div.displayArea.disableAnimations.fitToScreen > div.visualContainerHost > visual-container-repeat > visual-container-modern:nth-child(43) > transform > div > div:nth-child(4) > div > detail-visual-modern > div > visual-modern > div > div")
 
-#Scroll to desired section of rows. Appears that you can grab up to 30 rows per
-#scrape, however one should test by reviewing the number of rows in the rownames_2
-#object before proceeding.
-
 col_headers_2 <- pivot_data_table_2 %>%
   querySelectorAll("div.columnHeaders div.pivotTableCellWrap") %>%
   map_chr(xml_text) %>% 
@@ -120,16 +116,92 @@ casesPer_df <- municipality %>%
 sheet_write(casesPer_df, ss, date$date)
 
 
+#--------------------Total Cases by Municipality----------------------#
+
+# Right-click on total cases trend chart and choose `show as table`
+
+# fetch the site source in XML
+pivot_data_table_3 <- read_html(remDr$getPageSource()[[1]]) %>%
+  querySelector("#pvExplorationHost > div > div > exploration > div > explore-canvas-modern > div > div.canvasFlexBox > div > div.displayArea.disableAnimations.fitToScreen > div.visualContainerHost > visual-container-repeat > visual-container-modern:nth-child(5) > transform > div > div:nth-child(4) > div > detail-visual-modern > div > visual-modern > div > div")
+
+col_headers_3 <- pivot_data_table_3 %>%
+  querySelectorAll("div.columnHeaders div.pivotTableCellWrap") %>%
+  map_chr(xml_text) %>% 
+  str_trim("right")
+
+rownames_3 <- pivot_data_table_3 %>%
+  querySelectorAll("div.pivotTable div.rowHeaders div.pivotTableCellWrap") %>%
+  map_chr(xml_text)
+
+pivottable_data_3 <- pivot_data_table_3 %>%
+  querySelectorAll("div.bodyCells div.pivotTableCellWrap") %>%
+  map(xml_parent) %>%
+  unique() %>%
+  map(~ .x %>% querySelectorAll("div.pivotTableCellWrap") %>% map_chr(xml_text)) %>%
+  setNames(col_headers_3) %>%
+  bind_cols()
+
+municipality3 <- as.data.frame(rownames_3) %>% 
+  rename(Municipality = "rownames_3")
+
+# produce final data frame to write to csv
+totalCases_df <- municipality3 %>%
+  bind_cols(pivottable_data_3) %>% 
+  arrange(-desc(municipality))
+
+# add a sheet to google sheet named totalCases with today's date
+sheet_write(totalCases_df, ss3, date$date)
+
+
+#--------------------Total VoC Cases by Municipality----------------------#
+
+# Right-click on total cases trend chart and choose `show as table`
+
+# fetch the site source in XML
+pivot_data_table_4 <- read_html(remDr$getPageSource()[[1]]) %>%
+  querySelector("#pvExplorationHost > div > div > exploration > div > explore-canvas-modern > div > div.canvasFlexBox > div > div.displayArea.disableAnimations.fitToScreen > div.visualContainerHost > visual-container-repeat > visual-container-modern:nth-child(75) > transform > div > div:nth-child(4) > div > detail-visual-modern > div > visual-modern > div > div")
+
+
+col_headers_4 <- pivot_data_table_4 %>%
+  querySelectorAll("div.columnHeaders div.pivotTableCellWrap") %>%
+  map_chr(xml_text) %>% 
+  str_trim("right")
+
+rownames_4 <- pivot_data_table_4 %>%
+  querySelectorAll("div.pivotTable div.rowHeaders div.pivotTableCellWrap") %>%
+  map_chr(xml_text)
+
+pivottable_data_4 <- pivot_data_table_4 %>%
+  querySelectorAll("div.bodyCells div.pivotTableCellWrap") %>%
+  map(xml_parent) %>%
+  unique() %>%
+  map(~ .x %>% querySelectorAll("div.pivotTableCellWrap") %>% map_chr(xml_text)) %>%
+  setNames(col_headers_4) %>%
+  bind_cols()
+
+municipality4 <- as.data.frame(rownames_4) %>% 
+  rename(Municipality = "rownames_4")
+
+# produce final data frame to write to csv
+totalVOCCases_df <- municipality4 %>%
+  bind_cols(pivottable_data_4) %>% 
+  arrange(-desc(municipality))
+
+
+# add a sheet to google sheet named totalCases with today's date
+sheet_write(totalVOCCases_df, ss4, date$date)
+
+
 # ******* Google sheet code below *******
 # create new Google sheet
 # code below has to be done first and then just use the overwrite code
-#gs4_create(name = "weeklyCaseData", sheets = weeklyCaseData)
+gs4_create(name = "totalVOCCases", sheets = totalVOCCases_df)
 
 # read in existing Google workbook sheet link
 # code below has to be done second and then just use the add a sheet code
 #ss <- ("https://docs.google.com/spreadsheets/d/14h4wj0KnHE_9iBn5jCQIyQ2Lx9ef2gk1B7WSqjgmqWg/edit?usp=sharing")
 #ss2 <- ("https://docs.google.com/spreadsheets/d/1oXuLEH5dfsddpuOwgRmIS96tN6idGKqMLOmQhGcAGIk/edit?usp=sharing")
-
-
+#ss3 <- ("https://docs.google.com/spreadsheets/d/1WapXcSK1QAc0pwzztW7Zh1BV6ReWsYOuxwOZlKLaLVM/edit?usp=sharing")
+#ss4 <- ("https://docs.google.com/spreadsheets/d/1uCkASOSnikq5ShTEJcRRaTDr-eOm1qN120imIoIdd_4/edit?usp=sharing")
 
 
